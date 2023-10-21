22a17c9d971c2b24204d0543f4f440902f2d3f8f
gpjen95 <nyonggpj9995@gmail.com>
2023-10-21 16:01:52 +0900

    define a square

---
+++ aSquareOfSquares.js
@@ -0,0 +1,16 @@
+export function isSquare(n) {
+  if (n < 0) {
+    return false;
+  }
+
+  //   iterasi manual
+  //   for (let i = 0; i * i <= n; i++) {
+  //     if (i * i === n) {
+  //       return true;
+  //     }
+  //   }
+
+  //   iterasi bawaan javascript
+  const sqrt = Math.sqrt(n);
+  return Number.isInteger(sqrt);
+}
